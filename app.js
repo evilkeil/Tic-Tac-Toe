319eae2d0d586e13220ee4efdded5638149ebb2b
evilkeil <114153280+evilkeil@users.noreply.github.com>
2023-08-27 16:34:18 +0900

    make logic to clearly determine whther its a win lose or a draw

--- app.js
+++ app.js
@@ -41,42 +41,65 @@ const game = (function (){
 
 
 
-function winCondition(m){
-    const gameBoard = game.getGameboard();
-     if(
-        (gameBoard[0]===m && gameBoard[1]===m && gameBoard[2]===m) ||
-        (gameBoard[3]===m && gameBoard[4]===m && gameBoard[5]===m) ||
-        (gameBoard[6]===m && gameBoard[7]===m && gameBoard[8]===m) ||
-        (gameBoard[2]===m && gameBoard[5]===m && gameBoard[8]===m) ||
-        (gameBoard[1]===m && gameBoard[4]===m && gameBoard[7]===m) ||
-        (gameBoard[0]===m && gameBoard[3]===m && gameBoard[6]===m) ||
-        (gameBoard[2]===m && gameBoard[4]===m && gameBoard[6]===m) ||
-        (gameBoard[0]===m && gameBoard[4]===m && gameBoard[8]===m) 
-    ){
-        return m;
-    }else{
-        return "draw"
-    }
-}
-    
+// function winCondition(m){
+//     const gameBoard = game.getGameboard();
+//      if(
+//         (gameBoard[0]===m && gameBoard[1]===m && gameBoard[2]===m) ||
+//         (gameBoard[3]===m && gameBoard[4]===m && gameBoard[5]===m) ||
+//         (gameBoard[6]===m && gameBoard[7]===m && gameBoard[8]===m) ||
+//         (gameBoard[2]===m && gameBoard[5]===m && gameBoard[8]===m) ||
+//         (gameBoard[1]===m && gameBoard[4]===m && gameBoard[7]===m) ||
+//         (gameBoard[0]===m && gameBoard[3]===m && gameBoard[6]===m) ||
+//         (gameBoard[2]===m && gameBoard[4]===m && gameBoard[6]===m) ||
+//         (gameBoard[0]===m && gameBoard[4]===m && gameBoard[8]===m) 
+//     ){
+//         return m;
+//     }else{
+//         return "draw"
+//     }
+// }
   
 
 
+function winCondition(){
+    const gameBoard = game.getGameboard();
+    const markers =["X","O"];
+    
+    const result = markers.reduce((acc,curr) =>{
+        if(
+            (gameBoard[0]===curr && gameBoard[1]===curr && gameBoard[2]===curr) ||
+            (gameBoard[3]===curr && gameBoard[4]===curr && gameBoard[5]===curr) ||
+            (gameBoard[6]===curr && gameBoard[7]===curr && gameBoard[8]===curr) ||
+            (gameBoard[2]===curr && gameBoard[5]===curr && gameBoard[8]===curr) ||
+            (gameBoard[1]===curr && gameBoard[4]===curr && gameBoard[7]===curr) ||
+            (gameBoard[0]===curr && gameBoard[3]===curr && gameBoard[6]===curr) ||
+            (gameBoard[2]===curr && gameBoard[4]===curr && gameBoard[6]===curr) ||
+            (gameBoard[0]===curr && gameBoard[4]===curr && gameBoard[8]===curr) 
+        ){
+            acc = curr
+        }
+        return acc;
+    },"draw")
+return result
+}
 
-game.markGameboard(4,"X");
-game.markGameboard(7,"X");
-game.markGameboard(5,"O");
-game.markGameboard(8,"O");
-game.markGameboard(3,"O");
 game.markGameboard(0,"X");
+game.markGameboard(1,"O");
+game.markGameboard(2,"X");
+game.markGameboard(3,"X");
+game.markGameboard(4,"O");
+game.markGameboard(5,"O");
 game.markGameboard(6,"O");
-game.markGameboard(1,"X");
-game.markGameboard(2,"O");
+game.markGameboard(7,"X");
+game.markGameboard(8,"X");
 
 console.log(game.getGameboard());
 
-console.log(winCondition("O"));
-console.log(winCondition("X"));
+
+
+
+console.log(winCondition());
+// console.log(winCondition("X"));
 
 
 
