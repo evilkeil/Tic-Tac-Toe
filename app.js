38d8f7e9cfe93e0982c5968ce553998092969f8b
evilkeil <114153280+evilkeil@users.noreply.github.com>
2023-08-27 12:11:26 +0900

    try make a simple logic to mark the gameboarrd with X and O

--- app.js
+++ app.js
@@ -10,11 +10,43 @@ const createPlayer = function(name,marker){
 }
 
 
-const player1 = createPlayer("kevin","X");
+// const player1 = createPlayer("kevin","X");
 
 
- player1.addScore();
+//  player1.addScore();
 
+
+ 
+const game = (function (){
+    let gameBoard = [null,null,null,null,null,null,null,null,null];
+    const markGameboard =(i,marker)=>{
+       if (gameBoard[i] === null) {
+        if(marker === "X"){
+            gameBoard[i]="X"
+        }else if (marker === "O"){
+            gameBoard[i] = "O"
+        }
+    }
+    }
+    const getGameboard =()=>{
+        return gameBoard;
+    }
+    
+    return{
+        markGameboard,
+        getGameboard
+    }
+
+})();
+
+
+console.log(game.getGameboard());
+
+game.markGameboard(5,"X");
+game.markGameboard(7,"O");
+console.log(game.getGameboard());
+game.markGameboard(7,"X")
+console.log(game.getGameboard());
 
 
 
